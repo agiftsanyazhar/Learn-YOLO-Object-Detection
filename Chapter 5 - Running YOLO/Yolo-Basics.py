e0e0acac799d49e79fd57f330a4213105e0f508f
Agiftsany Azhar <agiftsanyazhar@gmail.com>
2023-12-29 17:50:51 +0700

    chapter 5 done

---
+++ Chapter 5 - Running YOLO/Yolo-Basics.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("YOLO-Weights/yolov8l.pt")
+# results = model("Chapter 5 - Running YOLO/images/1.png", show=True)
+# results = model("Chapter 5 - Running YOLO/images/2.png", show=True)
+results = model("Chapter 5 - Running YOLO/images/3.png", show=True)
+cv2.waitKey(0)
